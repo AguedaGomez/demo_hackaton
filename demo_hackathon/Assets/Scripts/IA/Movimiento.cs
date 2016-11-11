6c4a8b9ea7257cda5ad7e7f029fb68a898194244
Agueda Gomez Cambronero <al285895@uji.es>
2016-11-11 17:14:28 +0100

    Carpetas y escenas

---
+++ demo_hackathon/Assets/Scripts/IA/Movimiento.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movimiento : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
