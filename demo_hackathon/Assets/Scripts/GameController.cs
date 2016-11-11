46fd48f8929c443a97eed5f5c73cd742f49dea6d
Agueda Gomez Cambronero <al285895@uji.es>
2016-11-11 21:21:13 +0100

    GameController crea los npc y los coloca en distintos carriles

---
+++ demo_hackathon/Assets/Scripts/GameController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    //Coger alto de la pantalla, dividirlo entre los personajes (los jugadores + 20)
+    private int numeroTotalPersonajes = 8;
+    private int espacioVerticalEntrePj = 1; //calcular según la cantidad de personajes y el alto de la escena
+    private SpriteRenderer sprite;
+    public GameObject npc;
+    
+
+	// Use this for initialization
+	void Start () {
+        
+
+        for (int i = 0; i < numeroTotalPersonajes; i++)
+        {
+            Instantiate(npc);
+            npc.transform.position = new Vector3(0, 5-espacioVerticalEntrePj*i); //cambiar por el margen inferior
+            sprite = npc.GetComponent<SpriteRenderer>();
+            sprite.sortingOrder = i;
+            Debug.Log(npc.transform.position);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
